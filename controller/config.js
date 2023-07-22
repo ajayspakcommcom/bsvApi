cd79f9557d2b50a3b292c64e1e0344abc2ccbf88
Ajay Mahavir Vishwakarma <ajayspak@spakcomm.com>
2023-07-22 16:36:18 +0530

    latest changes

--- controller/config.js
+++ controller/config.js
@@ -1,9 +1,26 @@
+// exports.dataBaseConfig = {
+//   server: "P3NWPLSK12SQL-v15.shr.prod.phx3.secureserver.net",
+//   user: "spakDb",
+//   password: "Spak@123-",
+//   port: 1433,
+//   database: "bsvDb",
+//   pool: {
+//     max: 10,
+//     min: 0,
+//     idleTimeoutMillis: 30000,
+//   },
+//   options: {
+//     encrypt: true, // for azure
+//     trustServerCertificate: true, //change to true for local dev / self-signed certs
+//   },
+// };
+
 exports.dataBaseConfig = {
-  server: "P3NWPLSK12SQL-v15.shr.prod.phx3.secureserver.net",
-  user: "spakDb",
-  password: "Spak@123-",
+  server: "N1NWPLSK12SQL-v01.shr.prod.ams1.secureserver.net",
+  user: "hae",
+  password: "Spak@12345",
   port: 1433,
-  database: "bsvDb",
+  database: "Haemat",
   pool: {
     max: 10,
     min: 0,
